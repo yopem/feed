172b23a2196069a063757ea712f65169a74bea37
Yandi <karyana@yandi.me>
2025-12-04 18:44:26 +0700

    fix(login-button): use camelCase for SVG props
    Update SVG <g> element props to use camelCase (strokeLinecap, strokeLinejoin)
    as required by React.

--- src/components/auth/login-button.tsx
+++ src/components/auth/login-button.tsx
@@ -15,8 +15,8 @@ const LoginButton = () => {
           <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
           <g
             id="SVGRepo_tracerCarrier"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           ></g>
           <g id="SVGRepo_iconCarrier">
             <path
