ceb22762ad1692b9270e2b855fa01152d81d3b60
Yandi <karyana@yandi.me>
2025-12-06 14:19:46 +0700

    fix(scripts): update analytics attribute for website id
    Replace data-domain with data-website-id to ensure correct tracking.

--- src/components/scripts.tsx
+++ src/components/scripts.tsx
@@ -3,7 +3,7 @@ const Scripts = () => {
     <>
       <script
         defer
-        data-domain="yopem.com"
+        data-website-id="232ba2bd-2a81-40b6-9376-ca2259b10dc9"
         src="https://analytics.yopem.com/js/script.js"
       ></script>
     </>
