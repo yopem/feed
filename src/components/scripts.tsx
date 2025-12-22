50c8d3bd49056972bd926e59c618a92b57b1b456
Yandi <karyana@yandi.me>
2025-12-23 02:49:26 +0700

    fix(scripts): update analytics script src URL
    Change script src to use /script.js instead of /js/script.js for analytics.

--- src/components/scripts.tsx
+++ src/components/scripts.tsx
@@ -3,8 +3,8 @@ const Scripts = () => {
     <>
       <script
         defer
+        src="https://analytics.yopem.com/script.js"
         data-website-id="232ba2bd-2a81-40b6-9376-ca2259b10dc9"
-        src="https://analytics.yopem.com/js/script.js"
       ></script>
     </>
   )
