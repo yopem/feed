7d51e15b861cd0675266d6c899d90531bab238c3
Yandi <karyana@yandi.me>
2025-12-25 20:28:03 +0700

    feat(api): enable feed, tag, and user routers
    Uncomment and add feed, tag, and user routers to appRouter for API access.

--- src/lib/api/root.ts
+++ src/lib/api/root.ts
@@ -1,14 +1,13 @@
 import { articleRouter } from "./routers/article"
-
-// import { feedRouter } from "./routers/feed"
-// import { tagRouter } from "./routers/tag"
-// import { userRouter } from "./routers/user"
+import { feedRouter } from "./routers/feed"
+import { tagRouter } from "./routers/tag"
+import { userRouter } from "./routers/user"
 
 export const appRouter = {
   article: articleRouter,
-  // feed: feedRouter,
-  // tag: tagRouter,
-  // user: userRouter,
+  feed: feedRouter,
+  tag: tagRouter,
+  user: userRouter,
 }
 
 export type AppRouter = typeof appRouter
